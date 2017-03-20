6e50c85c0b6cd372ffc2ecc64700256d3d6fa7e9
ervitis <vitomarti@gmail.com>
2017-03-20 22:32:29 +0100

    feat: ch5 stream take toList

---
+++ src/main/scala/chapter5/Exercise5_2.scala
@@ -0,0 +1,48 @@
+package chapter5
+
+import skel.Skel
+
+import Stream._
+
+trait Stream[+A] {
+  def toList: List[A] = {
+    @annotation.tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h, t) => go(t(), h() :: acc)
+      case _ => acc
+    }
+
+    go(this, List()).reverse
+  }
+
+  def take(n: Int): Stream[A] = n match {
+    case i if i <= 0 => empty
+    case _ => this match {
+      case Empty => Empty
+      case Cons(h, t) => cons(h(), t().take(n - 1))
+    }
+  }
+}
+
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def empty[A]: Stream[A] = Empty
+
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+}
+
+class Exercise5_2 extends Skel {
+  override def execute() = {
+
+  }
+}
---
+++ src/test/scala/chapter5/Exercise5_2Test.scala
@@ -0,0 +1,23 @@
+package chapter5
+
+import interfaces.ScalaTestingBase
+
+class Exercise5_2Test extends ScalaTestingBase {
+  describe("streams") {
+    val s = Stream(2, 3, 4, 5, 6, 1, 2, 3)
+
+    it("returns a list") {
+      assert(s.toList === List(2, 3, 4, 5, 6, 1, 2, 3))
+    }
+
+    it("returns an empty list") {
+      assert(Stream().toList === List())
+    }
+
+    it("take function") {
+      val ns = s.take(3).toList
+
+      assert(ns === List(2, 3, 4))
+    }
+  }
+}
