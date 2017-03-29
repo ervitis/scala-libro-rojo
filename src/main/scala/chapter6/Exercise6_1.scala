55432a1798301be7496179ff4925c56f034d0c47
ervitis <vitomarti@gmail.com>
2017-03-29 21:07:51 +0200

    feat: state machine example

--- src/main/scala/chapter6/Exercise6_1.scala
+++ src/main/scala/chapter6/Exercise6_1.scala
@@ -50,6 +50,82 @@ object RNG {
   }
 }
 
+case class State[S, +A](run: S => (A, S)) {
+  import State._
+
+  def map[B](f: A => B): State[S, B] = flatMap(a => unit(f(a)))
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = flatMap(a => sb.map(b => f(a, b)))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a).run(s1)
+  })
+
+  def get[S]: State[S, S] = State(s => (s, s))
+}
+
+object State {
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] = {
+    def go(s: S, actions: List[State[S, A]], acc: List[A]): (List[A], S) = actions match {
+      case Nil => (acc.reverse, s)
+      case h :: t => h.run(s) match {
+        case (a, s2) => go(s2, t, a :: acc)
+      }
+    }
+    State((s: S) => go(s, sas, List()))
+  }
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+    val (a, rng2) = s(rng)
+    (f(a), rng2)
+  }
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, rng1) = f(rng)
+    g(a)(rng1)
+  }
+
+}
+
+sealed trait Input
+case object Coin extends Input
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+object CandyMachine {
+  import State._
+
+  def update = (i: Input) => (s: Machine) => (i, s) match {
+    case (_, Machine(_, 0, _)) => s
+    case (Coin, Machine(false, _, _)) => s
+    case (Coin, Machine(true, _, _)) => s
+    case (Coin, Machine(true, candy, coin)) => Machine(locked = false, candy, coin + 1)
+    case (Coin, Machine(false, candy, coin)) => Machine(locked = true, candy - 1, coin)
+  }
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- sequence(inputs map (modify[Machine] _ compose update))
+    s <- get
+  } yield (s.coins, s.candies)
+}
+
 class Exercise6_1 extends Skel {
   def execute(): Unit = {
 
